b6e219bc75d9cf6d811fb21b34191fcd73c66655
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-07-02 00:46:44 +0800

    Add button delete

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,9 +1,11 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {Card} from "../Card";
 import {useAppDispatch, useAppSelector} from "../../hooks/redux";
 import {deleteCategory, setActiveId } from '../../store/reducer/categorySlice';
 
 export const Categories: FC = () => {
+
+    const [isShow, setIsShow] = useState(false)
 
     const dispatch = useAppDispatch()
     const {categories, isActiveId} = useAppSelector(state => state.categories)
@@ -11,6 +13,8 @@ export const Categories: FC = () => {
     useEffect(() => {
         const onKeypress = (e: KeyboardEvent) => {
             if (e.key === "Delete") {
+                console.log(isActiveId)
+                dispatch(deleteCategory({id: isActiveId}))
             }
         };
         document.addEventListener('keydown', onKeypress);
@@ -19,13 +23,30 @@ export const Categories: FC = () => {
             document.removeEventListener('keydown', onKeypress);
             document.removeEventListener('keyup', onKeypress);
         };
-    }, [categories]);
+    }, [isActiveId]);
 
-    const onActiveClass = (id: string) => id === isActiveId ? dispatch(setActiveId({id: ""})) : dispatch(setActiveId({id}));
+    const onActiveClass = (id: string) => {
+        if (id === isActiveId) {
+            dispatch(setActiveId({id: ""}))
+            setIsShow(false)
+        } else {
+            dispatch(setActiveId({id}))
+            setIsShow(true)
+        }
+    }
+
+    const deleteButtonHandler = () => {
+        dispatch(deleteCategory({id: isActiveId}))
+        setIsShow(false)
+    }
 
     return (
         <div className="categories">
-            <button onClick={() => dispatch(deleteCategory({id: isActiveId}))}>Удалить</button>
+            <div className="categories__delete-item">
+                <button className={isShow ? "card__del-button card__del-button--active": "card__del-button"} onClick={() => deleteButtonHandler()}>
+                    Удалить
+                </button>
+            </div>
             <div className="categories__content-cards">
                 {
                     categories.map( el =>
@@ -34,11 +55,11 @@ export const Categories: FC = () => {
                             onClick={() => onActiveClass(el.id)}
                         >
                             <Card
-                            id={el.id}
-                            categoryName={el.categoryName}
-                            title={el.title}
-                            key={el.id}
-                        />
+                                id={el.id}
+                                categoryName={el.categoryName}
+                                title={el.title}
+                                key={el.id}
+                            />
                         </div>
                     )
                 }
