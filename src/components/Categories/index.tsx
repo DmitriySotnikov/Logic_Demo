c5538f47128ab7d88c08edd1cbf7b61a5e8ba5dc
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-09-11 22:29:16 +0800

    fix sort

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import {Card} from "../Card";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { deleteCategory, selectionCategory, setActiveId } from '../../store/reducer/categorySlice';
@@ -7,19 +7,6 @@ export const Categories: FC = () => {
 
     const dispatch = useAppDispatch();
     const {filteredCategories, isActiveId, selector, isLoading} = useAppSelector(state => state.categories);
-
-    useEffect(() => {
-        const onKeypress = (e: KeyboardEvent) => {
-            if (e.key === "Delete") {
-                deleteButtonHandler()
-            }
-        };
-        document.addEventListener('keydown', onKeypress);
-        return () => {
-            document.removeEventListener('keydown', onKeypress);
-            document.removeEventListener('keyup', onKeypress);
-        };
-    }, [isActiveId]);
 
     if (isLoading){
         return <h1 style={{display: "flex", justifyContent: "center"}}>Загрузка...</h1>
@@ -40,6 +27,9 @@ export const Categories: FC = () => {
                         <div
                             className={el.id === isActiveId ? "card card--active" : "card"} key={el.id}
                             onClick={() => onActiveClass(el.id)}
+                            onKeyDown={event => event.key==="Delete" && deleteButtonHandler()}
+                            role="contentinfo"
+                            tabIndex={0}
                         >
                             <Card
                                 category={el}
