f77f168facc3c724ff02d3f871e85a10af669524
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2022-09-11 22:55:36 +0800

    fix sort

--- src/components/Sort/index.tsx
+++ src/components/Sort/index.tsx
@@ -1,6 +1,6 @@
-import React, {FC} from 'react';
+import React, {FC} from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
-import { selectionCategory, sortCategories } from '../../store/reducer/categorySlice';
+import { selectionCategory, sortCategories } from "../../store/reducer/categorySlice";
 
 export const Sort: FC = () => {
 
@@ -8,15 +8,19 @@ export const Sort: FC = () => {
     const {selector} = useAppSelector(state => state.categories);
 
     const selectHandler = (e:  React.ChangeEvent<HTMLSelectElement>) => {
-        const key: string = e.target.value
-        dispatch(sortCategories(key))
-        dispatch(selectionCategory(selector))
+        const key: string = e.target.value;
+        dispatch(sortCategories(key));
+        dispatch(selectionCategory(selector));
     };
 
     return (
         <div className="sort">
             <form className="sort__form">
-                    <select className="selectors__select" onChange={e => selectHandler(e)} defaultValue={""}>
+                    <select
+                        className="selectors__select"
+                        onChange={e => selectHandler(e)}
+                        defaultValue=""
+                    >
                         <option value="" disabled>Выберите сортировку</option>
                         <option value="date">По дате</option>
                         <option value="title">По названию</option>
